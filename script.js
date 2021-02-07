e4f14635a0e2bc006f76865cdd92002fddb8f00a
Mostary21 <fkfmkhan1@gmail.com>
2021-02-08 05:15:29 +0600

    searchbox

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-.then(res => res.json())
-.then(data => displyMeal(data));
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => displayMeals(data));
 
-console.log(data);
+const displayMeals = (meals) =>{
+    const mealsDiv = document.getElementById('meals');
+    meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.className = 'meal';
+        const mealInfo = `
+        <img src="${meal.img}>
+        <h3 class="item-name">${meal.name}</h3>
+        `;
+        mealDiv.innerHTML = mealInfo;
+        mealsDiv.appendChild(mealDiv);
+    });
+}
